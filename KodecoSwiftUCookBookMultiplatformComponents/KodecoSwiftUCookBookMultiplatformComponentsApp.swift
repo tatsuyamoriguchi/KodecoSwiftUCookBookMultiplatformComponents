de80d1a29364df69f32b4833ead77bc4c9418599
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-06 08:07:28 -0700

    Initial Commit

---
+++ KodecoSwiftUCookBookMultiplatformComponents/KodecoSwiftUCookBookMultiplatformComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodecoSwiftUCookBookMultiplatformComponentsApp.swift
+//  KodecoSwiftUCookBookMultiplatformComponents
+//
+//  Created by Tatsuya Moriguchi on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct KodecoSwiftUCookBookMultiplatformComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
