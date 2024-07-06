d6d80aa3042996873078293214e290f08de6743b
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-07-06 08:29:31 -0700

    Multi platform

--- KodecoSwiftUCookBookMultiplatformComponents/ContentView.swift
+++ KodecoSwiftUCookBookMultiplatformComponents/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Tab 1")
+                .tabItem { Label("Tab 1", systemImage: "1.circle")
+                }
+            Text("Tab 2")
+                .tabItem { Label("Tab 2", systemImage: "2.circle") }
+        
         }
-        .padding()
     }
 }
 
---
+++ KodecoSwiftUCookBookMultiplatformComponents/Platform-SpecificCodeUsingConditionalCompilation.swift
@@ -0,0 +1,18 @@
+//
+//  Platform-SpecificCodeUsingConditionalCompilation.swift
+//  KodecoSwiftUCookBookMultiplatformComponents
+//
+//  Created by Tatsuya Moriguchi on 7/6/24.
+//
+
+import SwiftUI
+
+struct Platform_SpecificCodeUsingConditionalCompilation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Platform_SpecificCodeUsingConditionalCompilation()
+}
